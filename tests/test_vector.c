d17d70988835b843dfa9885ed2b0c0c4ce412472
Stefano Picchioni <stefanopicchio94@outlook.it>
2026-05-31 13:02:13 +0200

    added new tests and functions to orbit

--- tests/test_vector.c
+++ tests/test_vector.c
@@ -1,11 +1,7 @@
+#include "test.h"
+#include "constants.h"
 #include "vector.h"
 #include <math.h>
-
-#define TOL 1e-6
-#define TEST_START(name) int failed = 0; printf("[    RUN   ] %s\n", name)
-#define EXPECT_TRUE(cond, msg) if (!(cond)) { printf("[  FAILED  ] %s (Line %d)\n", msg, __LINE__); failed = 1; }
-#define EXPECT_NEAR(a, b, tol, msg) if (fabs((a) - (b)) > (tol)) { printf("[  FAILED  ] %s (Line %d: %f != %f)\n", msg, __LINE__, a, b); failed = 1; }
-#define TEST_END(name) if (!failed) { printf("[    OK    ] %s\n", name); } else { printf("[  FAILED  ] %s\n", name); }
 
 int test_vector_magnitude()
 {
@@ -14,7 +10,7 @@ int test_vector_magnitude()
     vec3 v = VEC3_IN_M(3.0, 4.0, 0.0);
     double result = mag(v);
 
-    EXPECT_NEAR(result, 5.0, TOL, "Magnitude math is inaccurate for vector [3, 4, 0]");
+    EXPECT_NEAR(result, 5.0, TOL, "Magnitude math is inaccurate for vector  v = [3, 4, 0]");
 
     vec3 zero = VEC3_ZERO_M();
     EXPECT_NEAR(mag(zero), 0.0, TOL, "Zero vector magnitude must be 0.0");
@@ -32,7 +28,6 @@ int test_vector_dot_product()
     vec3 v2 = VEC3_IN_M(0.0, 1.0, 0.0);
     double result = dot(v1, v2);
 
-    // not true its 0.0 but for sake of test
     EXPECT_NEAR(result, 0.0, TOL, "Dot product math failed for perpendicular vectors");
 
     TEST_END("test_vector_dot_product");
@@ -40,21 +35,71 @@ int test_vector_dot_product()
     return failed;
 }
 
-int main()
+int test_vector_dot_product_UNIT_MISMATCH_FAIL()
 {
-    printf("[==========] Running SDynamix Test Suite...\n");
-    int total_failures = 0;
+    TEST_START("test_vector_dot_product_UNIT_MISMATCH_FAIL");
 
-    // Run your vector tests here
-    total_failures += test_vector_magnitude();
-    total_failures += test_vector_dot_product();
+    vec3 v1 = VEC3_IN_M(1.0, 0.0, 0.0);
+    vec3 v2 = VEC3_IN_M(0.0, 1.0, 0.0);
+    v1.unit = KM;
+    double result = dot(v1, v2);
 
-    printf("[==========] Test Suite Finished.\n");
-    if (total_failures == 0) {
-        printf("[  PASSED  ] All math properties validated.\n");
-        return 0; // CTest reads 0 as absolute success
-    } else {
-        printf("[  FAILED  ] Test blocks broke constraints.\n");
-        return 1; // Return non-zero to signal a failure to CMake/CI
-    }
+    EXPECT_TRUE(isnan(result), "Result of dot product should be NAN on unit mismatch");
+
+    TEST_END("test_vector_dot_product_UNIT_MISMATCH_FAIL");
+
+    return failed;
+}
+
+int test_vector_sum()
+{
+    TEST_START("test_vector_sum");
+
+    vec3 v1 = VEC3_IN_M(1.0, 2.0, 5.0);
+    vec3 v2 = VEC3_IN_M(1.5, 1.0, 0.0);
+    vec3 result = sum(v1, v2);
+
+    EXPECT_NEAR(result.x, 2.5, TOL, "Property x was incorrect");
+    EXPECT_NEAR(result.y, 3.0, TOL, "Property y was incorrect");
+    EXPECT_NEAR(result.z, 5.0, TOL, "Property z was incorrect");
+
+    TEST_END("test_vector_sum");
+
+    return failed;
+}
+
+int test_vector_sum_UNIT_MISMATCH_FAIL()
+{
+    TEST_START("test_vector_sum_UNIT_MISMATCH_FAIL");
+
+    vec3 v1 = VEC3_IN_M(1.0, 2.0, 5.0);
+    vec3 v2 = VEC3_IN_M(1.5, 1.0, 0.0);
+    v1.unit = KM;
+    vec3 result = sum(v1, v2);
+
+    EXPECT_TRUE(isnan(result.x), "Vector x should be contaminated with NAN");
+    EXPECT_TRUE(isnan(result.y), "Vector y should be contaminated with NAN");
+    EXPECT_TRUE(isnan(result.z), "Vector z should be contaminated with NAN");
+    EXPECT_EQUAL(result.unit, INVALID_UNIT, "Vector unit state should be flagged with INVALID_UNIT");
+
+    TEST_END("test_vector_sum_UNIT_MISMATCH_FAIL");
+
+    return failed;
+}
+
+int run_vector_tests()
+{
+    int domain_test_failures = 0;
+
+    domain_test_failures += test_vector_magnitude();
+    printf("\n");
+    domain_test_failures += test_vector_dot_product();
+    printf("\n");
+    domain_test_failures += test_vector_dot_product_UNIT_MISMATCH_FAIL();
+    printf("\n");
+    domain_test_failures += test_vector_sum();
+    printf("\n");
+    domain_test_failures += test_vector_sum_UNIT_MISMATCH_FAIL();
+
+    return domain_test_failures;
 }
